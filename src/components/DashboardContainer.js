eaf9b1adb56eab8fb519dcdf716d0fb1699511d3
arkstudiodevelopers <nicksonogm@gmail.com>
2024-10-08 18:07:37 +0300

    Started on dashboards

---
+++ src/components/DashboardContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function DashboardContainer() {
+
+  return (
+    <div className='Dashboard-container'>
+        
+        
+        
+        {/* <div className='Cards'>
+            <h4>Top Keywords Ranked</h4>
+        </div>
+
+        <div className='Cards'>
+            <h4>Traffic Sources</h4>
+        </div>
+
+        <div className='Cards'>
+            <h4>Eastimated Monthly Visits</h4>
+        </div>
+
+        <div className='Cards'>
+            <h4>Engagements</h4>
+        </div>
+
+        <div className='Cards'>
+            <h4>Top Country Shares</h4>
+        </div>
+
+        <div className='Cards'>
+            <h4>Competitors</h4>
+        </div> */}
+    </div>
+  )
+}
+
+export default DashboardContainer
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function SearchBox() {
   const handleSearch = (e) => {
@@ -10,11 +10,21 @@ function SearchBox() {
     console.log(searchTerm)
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch(e);
+    }
+  };
+
   return (
     <div className="Search-box">
-      <input type="search" id="search-input" placeholder="Search URL..." />
-      {/* <button id="search-button" onClick={handleSearch}>Search</button> */}
-      <FontAwesomeIcon icon={faSearch} size="2x"className='Search-icon' onClick={handleSearch}/>
+      <input 
+        type="search" 
+        id="search-input" 
+        placeholder="Search URL..." 
+        onKeyPress={handleKeyPress} 
+      />
+      <FontAwesomeIcon icon={faSearch} size="2x" className='Search-icon' onClick={handleSearch}/>
     </div>
   );
 }

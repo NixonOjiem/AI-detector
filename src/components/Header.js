4847b4279a523b47b52ef4060bea4bcfe592d2f8
arkstudiodevelopers <nicksonogm@gmail.com>
2024-10-10 20:14:37 +0300

    To change the name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='Navigation-bar'>
-        <h2 className='App-heading'>Web Analytics Tool</h2>
+        <h2 className='App-heading'>To Be Tool</h2>
         <ul className='List-items'>
             <li><a href=''>Git Hub </a></li>
             <li><a href=''>Portfolio</a></li>
